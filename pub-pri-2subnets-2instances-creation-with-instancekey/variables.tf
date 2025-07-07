ee5cc3db4b52ff7eb31540d5ede1533ae4695c6e
Vishal Bhardwaj <vishalbhardwajformal@gmail.com>
2025-07-07 17:25:54 +0530

    Added module: public-private | 2-subnets | 2-instances | instance-key

---
+++ pub-pri-2subnets-2instances-creation-with-instancekey/variables.tf
@@ -0,0 +1,18 @@
+variable "access_key" {
+  description = "Access key to AWS console"
+}
+variable "secret_key" {
+  description = "Secret key to AWS console"
+}
+variable "region" {
+  description = "AWS region"
+}
+
+variable "ami_id" {
+  description = "AMI ID for your EC2 instances"
+}
+
+variable "key_name" {
+  description = "Name of the existing AWS key pair to use for SSH"
+  type        = string
+}
